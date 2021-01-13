6dc6a360e2c03b4761e82ee90f020d91726e0a05
laura <77134323+6C61757261@users.noreply.github.com>
2021-01-12 20:21:56 -0800

    section 4

---
+++ 6C61757261_py_conditional_012021.py
@@ -0,0 +1,42 @@
+# 3.1 Write a program to prompt the user for hours and rate per hour using input to compute gross pay.
+# Pay the hourly rate for the hours up to 40 and 1.5 times the hourly rate for all hours worked above 40 hours.
+# Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75).
+# You should use input to read a string and float() to convert the string to a number.
+# Do not worry about error checking the user input - assume the user types numbers properly.
+
+hrs = int(input("Enter Hours:"))
+payRate = float(input("Enter pay Rate per hour: "))
+
+overtime = 0
+if hrs >= 40:
+    overtime = hrs - 40
+    hrs = 40
+
+grossPay = (hrs * payRate) + (overtime * (payRate*1.5))
+
+
+print(grossPay)
+
+# 3.3 Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error.
+# If the score is between 0.0 and 1.0, print a grade using the following table:
+# Score Grade
+# >= 0.9 A
+# >= 0.8 B
+# >= 0.7 C
+# >= 0.6 D
+# < 0.6 F
+# If the user enters a value out of range, print a suitable error message and exit. For the test, enter a score of 0.85.
+
+score = float(input("Enter Score: "))
+if score > 1.0 or score < 0:
+    print("Error. value should be between 0.0 and 1.0")
+elif score >= 0.9:
+    print("A")
+elif score >= 0.8:
+    print("B")
+elif score >= 0.7:
+    print("C")
+elif score >= 0.6:
+    print("D")
+else:
+    print("F")
