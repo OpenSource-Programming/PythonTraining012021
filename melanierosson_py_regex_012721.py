34ffb2febaccc3e312154866131696712ed8c5b0
Melanie Panem <16231664+melanierosson@users.noreply.github.com>
2021-01-23 10:14:00 -0800

    Update melanierosson_py_regex_012721.py

--- melanierosson_py_regex_012721.py
+++ melanierosson_py_regex_012721.py
@@ -1,47 +1,40 @@
-# taste the rainbow
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-# importing re????
+# importing re
 import re
 
 # opening file
 filename = 'regex_sum_1136998.txt'
 filetxt = open(filename)
 
-# creates dictionary out of file contents (unsorted)
-dictionary = dict()
-
-for line in filetxt : 
-	line = line.rstrip()
-	words = line.split()
-	for w in words:
-		dictionary[w] = dictionary.get(w,0) + 1
-
-# turns dictionary into strings
-stringFile = str(dictionary)
-
+# declarations
 regex = '[0-9]+'
 sumInts = 0  
 countInts = 0
+integers = []
+newIntList = []
+newIntItemStr = ''
 
-# finds all integers in the file
-integers = re.findall(regex, stringFile)
+# stripping whitespace and finding integers in each line of the file
+for line in filetxt : 
+	line = line.rstrip()
+	integers.append(re.findall(regex, line))
+    
+# removing empty list items in the file line lists (lines that had no integers)
+onlyInts = [listItems for listItems in integers if listItems]
 
-# counts and finds sum of integers
-for eachInt in integers :
-	eachInt = eachInt.rstrip()
+# stringifying everything (there are currently list items within the larger list)
+for eachItem in onlyInts :
+    for insideEach in eachItem :
+        newIntItemStr = newIntItemStr + ' ' + insideEach
+    newIntList = newIntItemStr
+    # splitting separate items into a single list based on spaces
+    numList = newIntList.split()
 
-	countInts = countInts + 1
-	sumInts = sumInts + int(eachInt)
+# getting final numbers from final integer list
+for numItems in numList :
+    # list item count
+    countInts += 1
+    # list items sum
+    sumInts += int(numItems)
 
-print(bcolors.WARNING + '\n💣  The ' + bcolors.HEADER + 'total number' + bcolors.WARNING + ' of integers in ' + bcolors.OKCYAN + filename + bcolors.WARNING + ' is ' + bcolors.OKGREEN + str(countInts) + bcolors.WARNING)
-print('💥  The ' + bcolors.HEADER + 'sum '  + bcolors.WARNING + 'of all the integers in ' + bcolors.OKCYAN + filename + bcolors.WARNING + ' is ' + bcolors.OKGREEN + str(sumInts) + bcolors.ENDC + '\n')
+print('The total integer count is ' +  str(countInts))
+print('The sum of all integers is ' + str(sumInts))
